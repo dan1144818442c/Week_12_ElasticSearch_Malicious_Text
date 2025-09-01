0e168a8771e4653c6faf7e69ae53807502dbe9b6
d.c! <daniel.c99999@gmail.com>
2025-09-01 16:10:10 +0300

    update dev

---
+++ elastic_serch.py
@@ -0,0 +1,81 @@
+from elasticsearch import Elasticsearch
+
+class ElasticSearch_:
+    def __init__(self):
+        self.es = Elasticsearch("http://localhost:9200")
+
+
+    def create_indexand(self, index_name, mappings):
+
+        # Make sure index exists (you can create with mapping before this)
+        if not self.es.indices.exists(index=index_name):
+            self.es.indices.create(index=index_name, body={
+                "mappings":
+                   mappings
+            })
+
+        print(f" Index  created")
+
+
+    def index_documents(self, data, index_name):
+        # Insert documents one by one
+        c = 0
+        for dic in data:  # data is your list[dict] from CSV
+            res = self.es.index(index=index_name, document=dic)
+            print(c)
+            c += 1
+        print("index doc")
+
+    def get_all_data(self , index_name):
+        query_all = {
+            "size": 10000,  # Adjust size as needed to retrieve all documents
+            "query": {
+                "match_all": {}
+            }
+        }
+
+        response = self.es.search(index=index_name, body=query_all)
+        documents = response['hits']['hits']
+        return documents
+
+
+    def update_all_documents_by_func(self, index_name, update_func):
+        """
+        update_func: function(doc: dict) -> dict
+        """
+        # קרא את כל המסמכים
+        docs = self.es.search(index=index_name, body={"query": {"match_all": {}}}, size=10000)
+        for hit in docs['hits']['hits']:
+            doc_id = hit['_id']
+
+            source = hit['_source']
+            new_doc = update_func(source)  # הפעל את הפונקציה על המסמך
+            self.es.update(index=index_name, id=doc_id, body={"doc": new_doc})
+
+
+
+    def update(self ,index_name , list_weapond):
+        update_query = {
+            "script": {
+                "lang": "painless",
+                "source": """
+                    if (ctx._source.weapons_detected == null) {
+                        ctx._source.weapons_detected = [];
+                    }
+                    for (w in params.weapons) {
+                        if (ctx._source.text.toLowerCase().contains(w)) {
+                            if (!ctx._source.weapons_detected.contains(w)) {
+                                ctx._source.weapons_detected.add(w);
+                            }
+                        }
+                    }
+                """,
+                "params": {"weapons": list_weapond}
+            },
+            "query": {
+                "match_all": {}  # or any filter you want
+            }
+        }
+
+        res = self.es.update_by_query(index=index_name, body=update_query, refresh=True)
+        print(res)
---
+++ tools.py
@@ -0,0 +1,69 @@
+import nltk
+import csv
+nltk.download('/usr/local/share/nltk_data')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+class Enricher:
+
+    @staticmethod
+    def weapons_detected(weapon_path:str,data:str):
+        weapon_list = Loader.load_from_txt_to_list(weapon_path)
+        clean_weapon_list = []
+        for weapon in weapon_list:
+            clean_weapon_list.append(weapon.lower())
+        weapons = []
+        for word in data.split():
+            if word in clean_weapon_list:
+                weapons.append(word)
+
+        return weapons
+
+    @staticmethod
+    def find_emotion_of_text( text: str):
+        score = SentimentIntensityAnalyzer().polarity_scores(text)
+        emotion = score['compound']
+        if 1 > emotion > 0.5:
+            return "positive"
+        elif 0.5 > emotion > -0.5:
+            return "neutral"
+        elif -0.5 > emotion > -1:
+            return "negative"
+        else:
+            return "?"
+
+    @staticmethod
+    def add_sentiment_to_dict(dict_):
+        emotion = Enricher.find_emotion_of_text(dict_["text"])
+        dict_["sentiment"] = emotion
+        return dict_
+
+
+class Loader :
+
+    @staticmethod
+    def load_from_txt_to_list(path):
+        try:
+            with open(path, "r") as weapons:
+                # read lines, strip newline, convert to lowercase
+                return [line.strip().lower() for line in weapons]
+        except FileNotFoundError:
+            print("didn't find this file")
+            return []
+
+    @staticmethod
+    def load_from_csv_to_list_of_dict(path):
+        data_list = []
+        with open(path, 'r' , encoding='utf-8', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data_list.append(row)
+
+        return data_list
+
+
+
+#
+# print(Loader.load_from_txt_to_list(
+#     r'C:\Users\1\Desktop\DATA_Analiza\Week_12_ElasticSearch_Malicious_Text\data\weapon_list.txt'))
+# print(Loader.load_from_csv_to_list_of_dict(r'C:\Users\1\Desktop\DATA_Analiza\Week_12_ElasticSearch_Malicious_Text\data\tweets_injected 3.csv'))
